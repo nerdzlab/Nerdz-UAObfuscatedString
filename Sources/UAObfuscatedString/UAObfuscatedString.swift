bc60819fc50b6ce3600dac4007955d8bbf710439
Vasyl Khmil <vasyl@nerdzlab.com>
2021-08-31 01:00:45 +0300

    - removed mutable string;

--- Sources/UAObfuscatedString/UAObfuscatedString.swift
+++ Sources/UAObfuscatedString/UAObfuscatedString.swift
@@ -37,35 +37,6 @@ public extension String {
     var z: String { return self + "z" }
 }
 
-@objc public extension NSMutableString {
-    var a: NSMutableString { append("a"); return self }
-    var b: NSMutableString { append("b"); return self }
-    var c: NSMutableString { append("c"); return self }
-    var d: NSMutableString { append("d"); return self }
-    var e: NSMutableString { append("e"); return self }
-    var f: NSMutableString { append("f"); return self }
-    var g: NSMutableString { append("g"); return self }
-    var h: NSMutableString { append("h"); return self }
-    var i: NSMutableString { append("i"); return self }
-    var j: NSMutableString { append("j"); return self }
-    var k: NSMutableString { append("k"); return self }
-    var l: NSMutableString { append("l"); return self }
-    var m: NSMutableString { append("m"); return self }
-    var n: NSMutableString { append("n"); return self }
-    var o: NSMutableString { append("o"); return self }
-    var p: NSMutableString { append("p"); return self }
-    var q: NSMutableString { append("q"); return self }
-    var r: NSMutableString { append("r"); return self }
-    var s: NSMutableString { append("s"); return self }
-    var t: NSMutableString { append("t"); return self }
-    var u: NSMutableString { append("u"); return self }
-    var v: NSMutableString { append("v"); return self }
-    var w: NSMutableString { append("w"); return self }
-    var x: NSMutableString { append("x"); return self }
-    var y: NSMutableString { append("y"); return self }
-    var z: NSMutableString { append("z"); return self }
-}
-
 // MARK: - A-Z -
 public extension String {
     var A: String { return self + "A" }
@@ -96,35 +67,6 @@ public extension String {
     var Z: String { return self + "Z" }
 }
 
-@objc public extension NSMutableString {
-    var A: NSMutableString { append("A"); return self }
-    var B: NSMutableString { append("B"); return self }
-    var C: NSMutableString { append("C"); return self }
-    var D: NSMutableString { append("D"); return self }
-    var E: NSMutableString { append("E"); return self }
-    var F: NSMutableString { append("F"); return self }
-    var G: NSMutableString { append("G"); return self }
-    var H: NSMutableString { append("H"); return self }
-    var I: NSMutableString { append("I"); return self }
-    var J: NSMutableString { append("J"); return self }
-    var K: NSMutableString { append("K"); return self }
-    var L: NSMutableString { append("L"); return self }
-    var M: NSMutableString { append("M"); return self }
-    var N: NSMutableString { append("N"); return self }
-    var O: NSMutableString { append("O"); return self }
-    var P: NSMutableString { append("P"); return self }
-    var Q: NSMutableString { append("Q"); return self }
-    var R: NSMutableString { append("R"); return self }
-    var S: NSMutableString { append("S"); return self }
-    var T: NSMutableString { append("T"); return self }
-    var U: NSMutableString { append("U"); return self }
-    var V: NSMutableString { append("V"); return self }
-    var W: NSMutableString { append("W"); return self }
-    var X: NSMutableString { append("X"); return self }
-    var Y: NSMutableString { append("Y"); return self }
-    var Z: NSMutableString { append("Z"); return self }
-}
-
 // MARK: - Numbers -
 public extension String {
     var _1: String { return self + "1" }
@@ -137,19 +79,6 @@ public extension String {
     var _8: String { return self + "8" }
     var _9: String { return self + "9" }
     var _0: String { return self + "0" }
-}
-
-@objc public extension NSMutableString {
-    var _1: NSMutableString { append("1"); return self }
-    var _2: NSMutableString { append("2"); return self }
-    var _3: NSMutableString { append("3"); return self }
-    var _4: NSMutableString { append("4"); return self }
-    var _5: NSMutableString { append("5"); return self }
-    var _6: NSMutableString { append("6"); return self }
-    var _7: NSMutableString { append("7"); return self }
-    var _8: NSMutableString { append("8"); return self }
-    var _9: NSMutableString { append("9"); return self }
-    var _0: NSMutableString { append("0"); return self }
 }
 
 // MARK: - Punctuation -
@@ -187,47 +116,9 @@ public extension String {
     var underscore: String { return self + "_" }
 }
 
-@objc public extension NSMutableString {
-    var space: NSMutableString { append(" "); return self }
-    var point: NSMutableString { append("."); return self }
-    var dash: NSMutableString { append("-"); return self }
-    var comma: NSMutableString { append(","); return self }
-    var semicolon: NSMutableString { append(";"); return self }
-    var colon: NSMutableString { append(":"); return self }
-    var apostrophe: NSMutableString { append("'"); return self }
-    var quotation: NSMutableString { append("\""); return self }
-    var plus: NSMutableString { append("+"); return self }
-    var equals: NSMutableString { append("="); return self }
-    var paren_left: NSMutableString { append("("); return self }
-    var paren_right: NSMutableString { append(")"); return self }
-    var asterisk: NSMutableString { append("*"); return self }
-    var ampersand: NSMutableString { append("&"); return self }
-    var caret: NSMutableString { append("^"); return self }
-    var percent: NSMutableString { append("%"); return self }
-    var `$`: NSMutableString { append("$"); return self }
-    var pound: NSMutableString { append("#"); return self }
-    var at: NSMutableString { append("@"); return self }
-    var exclamation: NSMutableString { append("!"); return self }
-    var question_mark: NSMutableString { append("?"); return self }
-    var back_slash: NSMutableString { append("\\"); return self }
-    var forward_slash: NSMutableString { append("/"); return self }
-    var curly_left: NSMutableString { append("{"); return self }
-    var curly_right: NSMutableString { append("}"); return self }
-    var bracket_left: NSMutableString { append("["); return self }
-    var bracket_right: NSMutableString { append("]"); return self }
-    var bar: NSMutableString { append("|"); return self }
-    var less_than: NSMutableString { append("<"); return self }
-    var greater_than: NSMutableString { append(">"); return self }
-    var underscore: NSMutableString { append("_"); return self }
-}
-
 // MARK: - Aliases -
 public extension String {
     var dot: String { return point }
-}
-
-@objc public extension NSMutableString {
-    var dot: NSMutableString { return point }
 }
 
 // swiftlint:enable identifier_name
